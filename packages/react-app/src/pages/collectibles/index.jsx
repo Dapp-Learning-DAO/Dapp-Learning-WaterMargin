a27f13c7a78885b167b86424fa3d24b238308080
xiangzhengfeng <eoszhengfeng@163.com>
2021-12-25 13:04:58 +0800

    add nft mint loading

--- packages/react-app/src/pages/collectibles/index.jsx
+++ packages/react-app/src/pages/collectibles/index.jsx
@@ -1,12 +1,13 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { Address, AddressInput } from "../../components";
 import { Modal, Button, Image, message, Tooltip } from "antd";
 import { activeColor, mainWidth, bgColor } from "../../theme";
 import StackGrid from "react-stack-grid";
 import errorImage from "../transfer/errorImge.jpg"
 import bgImage from "../../static/watermargin.png"
-import { LoadingCore } from "../../components/Loading"
+import { LoadingCore, useLoading } from "../../components/Loading"
 import { NoData } from "../../components/NoData";
+import { LoadingNFT } from "./LoadingNFT"
 
 export const YourCollectibles = (props) => {
   const {
@@ -30,22 +31,30 @@ export const YourCollectibles = (props) => {
 
   const blockExplorerLink = (contract, id) => `${blockExplorer || "https://etherscan.io/"}token/${contract}?a=${id}`;
   const [isModalVisible, setIsModalVisible] = useState(false);
+  //isShowLoading: Whether to display transition animations
+  const [isShowLoading, setLoading] = useState(localStorage.getItem("isMint") === "1");
   const [selectId, setSelectId] = useState();
+  const { loading } = useLoading()
 
   const handleOk = useCallback(() => {
     if (transferToAddresses[selectId]?.length !== 42 || transferToAddresses[selectId]?.indexOf("0x") !== 0) {
       message.warning("Please enter a well-formed address")
       return
     }
-    tx(
-      writeContracts.DappLearningCollectible.transferFrom(address, transferToAddresses[selectId], selectId),
-    );
-    setIsModalVisible(false)
+    tx(writeContracts.DappLearningCollectible.transferFrom(address, transferToAddresses[selectId], selectId)).then((res) => {
+      if (res) setIsModalVisible(false)
+    })
   }, [selectId, transferToAddresses, address, writeContracts]);
 
   const handleCancel = () => {
     setIsModalVisible(false);
   };
+
+  useEffect(() => {
+    if (currentColl?.data?.dappLearningCollectibles?.length > 0) {
+      localStorage.setItem("isMint", "0")
+    }
+  }, [currentColl?.data?.dappLearningCollectibles?.length])
 
   return (
     <div style={{ width: "100%", paddingBottom: 32 }}>
@@ -85,12 +94,12 @@ export const YourCollectibles = (props) => {
                 }}
                 disabled={address && getProof(address).length === 0 && web3Modal.cachedProvider}
                 onClick={() => {
-                  // 加了个首页可以连接钱包的功能
+                  // Added a home page to connect the function of the wallet
                   if (!web3Modal.cachedProvider) {
-                    loadWeb3Modal().then(()=>{
-                    // 如果不强刷，那么在调用loadWeb3Modal函数连接成功后，此时的 isInclaimList 值为false，所以mint按钮还是会显示出来。过一会儿后isInclaimList又变为了 true，mint又给隐藏了。
-                    // 所以显示了mint按钮，但是用户点击后发现没效果，后面又自动隐藏了，体验不好。而之前他们写的逻辑不敢乱动，所以进行强制刷新。这个问题一直存在，之前的逻辑不敢动，故仅在此处处理了以下。
-                    window.location.reload();
+                    loadWeb3Modal().then(() => {
+                      // If you do not force the brush, then after the loadWeb3Modal function is successfully connected, the isInclaimList value at this time is false, so the mint button will still be displayed. After a while isInclaimList became true again, and mint was hidden again.
+                      // So the mint button is displayed, but the user clicks and finds no effect, and then it is automatically hidden, and the experience is not good. The logic they wrote before did not dare to move, so it was forced to refresh. This problem has always existed, and the previous logic did not dare to move, so it only dealt with the following here.
+                      window.location.reload();
                     })
                   } else {
                     tx(
@@ -98,7 +107,27 @@ export const YourCollectibles = (props) => {
                         window.crypto.getRandomValues(new Uint32Array(1))[0],
                         getProof(address),
                       ),
-                    );
+                    ).then((res) => {
+                      // res.wait There is a delay of a few seconds after the transaction is successful
+                      /* if (res && res.wait) {
+                        res.wait().then(() => {
+                          localStorage.setItem("isMint", '1');
+                          setLoading(true)
+                        })
+                      } */
+                      // For the time being, only if the confirmation button is clicked, the transition animation is displayed
+                      if (res) {
+                        localStorage.setItem("isMint", '1');
+                        setLoading(true)
+                        if (res.wait) {
+                          res.wait().then().catch(() => {
+                            // tx fail
+                            localStorage.setItem("isMint", '0');
+                            setLoading(false)
+                          })
+                        }
+                      }
+                    })
                   }
                 }}
               >
@@ -112,91 +141,95 @@ export const YourCollectibles = (props) => {
           </div>
         </div>
       </div>
-      <div style={{ width: mainWidth, margin: "auto" }}>
+      <div style={{ width: mainWidth, margin: "auto", position: "relative" }}>
         {currentColl?.data?.dappLearningCollectibles?.length > 0 && <p>All you have WaterMargin NFT</p>}
-        {currentColl?.data?.dappLearningCollectibles?.length > 0 ? <StackGrid columnWidth={250} gutterWidth={20} gutterHeight={32} style={{ marginTop: 20 }}>
-          {currentColl?.data?.dappLearningCollectibles?.map((item, index) => {
-            if (!id_rank[item.tokenId]) return;
-            item = { ...item, ...assets[assetKeys[id_rank[item.tokenId]]] };
-            const id = id_rank[item.tokenId] * 1;
-            return (
-              <div
-                key={`${index}${id}`}
-                className="cardBox"
-                style={{
-                  background: bgColor,
-                  boxShadow: "10px 10px 10px rgba(0,0,0,0.5)",
-                  width: 250,
-                  height: 275,
-                  minHeight: 275,
-                  borderRadius: 5,
-                  border: `1px solid ${bgColor}`,
-                  textAlign: "left",
-                  color: "rgba(0,0,0,0.7)"
-                }}>
-                <Image
-                  width={250}
-                  height={186}
-                  preview={{ mask: null }}
-                  src={item?.image || errorImage}
-                  placeholder={
-                    <div style={{
-                      width: "100%",
-                      height: "100%",
+        {currentColl?.data?.dappLearningCollectibles?.length > 0
+          ? <StackGrid columnWidth={250} gutterWidth={20} gutterHeight={32} style={{ marginTop: 20 }}>
+            {currentColl?.data?.dappLearningCollectibles?.map((item, index) => {
+              if (!id_rank[item.tokenId]) return;
+              item = { ...item, ...assets[assetKeys[id_rank[item.tokenId]]] };
+              const id = id_rank[item.tokenId] * 1;
+              return (
+                <div
+                  key={`${index}${id}`}
+                  className="cardBox"
+                  style={{
+                    background: bgColor,
+                    boxShadow: "10px 10px 10px rgba(0,0,0,0.5)",
+                    width: 250,
+                    height: 275,
+                    minHeight: 275,
+                    borderRadius: 5,
+                    border: `1px solid ${bgColor}`,
+                    textAlign: "left",
+                    color: "rgba(0,0,0,0.7)"
+                  }}>
+                  <Image
+                    width={250}
+                    height={186}
+                    preview={{ mask: null }}
+                    src={item?.image || errorImage}
+                    placeholder={
+                      <div style={{
+                        width: "100%",
+                        height: "100%",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        background: "#F2F2F2"
+                      }}>
+                        <div style={{ marginLeft: -20, marginTop: -60 }}>
+                          <LoadingCore scale={2} />
+                        </div>
+                      </div>
+                    }
+                  />
+                  <a
+                    style={{
+                      fontSize: 20,
+                      borderRadius: "50%",
+                      background: "#FFFFFF",
+                      width: 40,
+                      height: 40,
                       display: "flex",
                       justifyContent: "center",
                       alignItems: "center",
-                      background: "#F2F2F2"
-                    }}>
-                      <div style={{ marginLeft: -20, marginTop: -60 }}>
-                        <LoadingCore scale={2} />
-                      </div>
+                      marginTop: -20,
+                      marginLeft: 105,
+                      cursor: "pointer",
+                      fontStyle: "italic",
+                      color: "rgba(0,0,0,0.7)",
+                      zIndex: 100
+                    }}
+                    target="_blank"
+                    href={blockExplorerLink(nftAddress, parseInt(item?.tokenId))}
+                  >{parseInt(item?.tokenId)}</a>
+                  <div style={{
+                    paddingLeft: 10,
+                    paddingRight: 10,
+                    marginTop: 10
+                  }}>
+                    <div style={{ display: "flex", justifyContent: "space-between" }}>
+                      {item?.description && <Tooltip title={item?.description}>
+                        <div className="ellipsis" style={{ fontSize: 16, marginRight: 8 }}>{item?.description}</div>
+                      </Tooltip>}
+                      <div style={{ width: 70, textAlign: "right" }}>{`Rank ${id + 1}`}</div>
                     </div>
-                  }
-                />
-                <a
-                  style={{
-                    fontSize: 20,
-                    borderRadius: "50%",
-                    background: "#FFFFFF",
-                    width: 40,
-                    height: 40,
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    marginTop: -20,
-                    marginLeft: 105,
-                    cursor: "pointer",
-                    fontStyle: "italic",
-                    color: "rgba(0,0,0,0.7)",
-                    zIndex: 100
-                  }}
-                  target="_blank"
-                  href={blockExplorerLink(nftAddress, parseInt(item?.tokenId))}
-                >{parseInt(item?.tokenId)}</a>
-                <div style={{
-                  paddingLeft: 10,
-                  paddingRight: 10,
-                  marginTop: 10
-                }}>
-                  <div style={{ display: "flex", justifyContent: "space-between" }}>
-                    {item?.description && <Tooltip title={item?.description}>
-                      <div className="ellipsis" style={{ fontSize: 16, marginRight: 8 }}>{item?.description}</div>
-                    </Tooltip>}
-                    <div style={{ width: 70, textAlign: "right" }}>{`Rank ${id + 1}`}</div>
-                  </div>
-                  <div style={{ color: activeColor, display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-                    <Address address={item?.owner} ensProvider={mainnetProvider} blockExplorer={blockExplorer} fontSize={14} size={6} disableBlockies disableCopy />
-                    <Button type="primary" style={{ width: 70 }} size="small" onClick={() => {
-                      setSelectId(parseInt(item?.tokenId))
-                      setIsModalVisible(true)
-                    }}>Transfer</Button>
+                    <div style={{ color: activeColor, display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                      <Address address={item?.owner} ensProvider={mainnetProvider} blockExplorer={blockExplorer} fontSize={14} size={6} disableBlockies disableCopy />
+                      <Button type="primary" style={{ width: 70 }} size="small" onClick={() => {
+                        setSelectId(parseInt(item?.tokenId))
+                        setIsModalVisible(true)
+                      }}>Transfer</Button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            )
-          })}
-        </StackGrid> : <NoData description={"You don't have any WaterMargin NFT!"} />}
+              )
+            })}
+          </StackGrid>
+          : (isShowLoading
+            ? (loading ? null : <LoadingNFT />)
+            : <NoData description={"You don't have any WaterMargin NFT!"} />)}
         <Modal
           title="Transfer WaterMargin NFT"
           okText={"Transfer"}
