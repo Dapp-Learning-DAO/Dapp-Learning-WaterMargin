fd8ea86804a96a0d3f66bd9941511d42b97b5c89
longdacao <longdacao@163.com>
2022-02-21 23:12:56 +0800

    change redpacket address

--- packages/react-app/src/contracts/HappyRedPacket.address.js
+++ packages/react-app/src/contracts/HappyRedPacket.address.js
@@ -1,2 +1,2 @@
-module.exports = "0x825d56a385BD4Dd1b03B9033Ccfe4a2B61edAacA"; //matic
+module.exports = "0x0253b677B62D3f45C83F43F41080abd970b22f8D"; //matic
 //module.exports = "0xE3aD032963F77Ed847aF68450824F306a44B5dFc"; //kovan
